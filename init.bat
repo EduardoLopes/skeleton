d005c237be87055e8e1bea9fecce0d939021cebf
Eduardo Lopes <EduardoLopesDS@gmail.com>
2013-12-19 02:48:31 -0300

    Update init.bat

--- init.bat
+++ init.bat
@@ -1,25 +1,25 @@
 REM @echo off
 echo "Initializing structure..."
-mkdir .\assets
-mkdir .\assets\images
-mkdir .\assets\css
-mkdir .\assets\js
-mkdir .\assets\js/vendor
+mkdir .\app
+mkdir .\app\img
+mkdir .\app\css
+mkdir .\app\js
+mkdir .\app\js\vendor
 
 echo "Copying files..."
 copy .\template\.editorconfig .\.editorconfig
 copy .\template\.gitignore .\.gitignore
 copy .\template\.jshintrc .\.jshintrc
 copy .\template\Gruntfile.js .\Gruntfile.js
-copy .\template\humans.txt .\humans.txt
-copy .\template\index.html .\index.html
+copy .\template\app\humans.txt .\app\humans.txt
+copy .\template\app\index.html .\app\index.html
 copy .\template\package.json .\package.json
-copy .\template\robots.txt .\robots.txt
-copy .\template\assets\css\style.css .\assets\css\style.css
-copy .\template\assets\css\normalize.css .\assets\css\normalize.css
-copy .\template\assets\js\main.js .\assets\js\main.js
-copy .\template\assets\js\vendor\jquery-1.10.2.min.js .\assets\js\vendor\jquery-1.10.2.min.js
-copy .\template\assets\js\vendor\modernizr-2.6.2.min.js .\assets\js\vendor\modernizr-2.6.2.min.js
+copy .\template\app\robots.txt .\app\robots.txt
+copy .\template\app\css\style.css .\app\css\style.css
+copy .\template\app\css\normalize.css .\app\css\normalize.css
+copy .\template\app\js\main.js .\app\js\main.js
+copy .\template\app\js\vendor\jquery-1.10.2.min.js .\app\js\vendor\jquery-1.10.2.min.js
+copy .\template\app\js\vendor\modernizr-2.6.2.min.js .\app\js\vendor\modernizr-2.6.2.min.js
 
 echo "Removing stuff you don't want..."
 del /S /F /Q .git
